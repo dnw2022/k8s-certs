a05f4e1fc599f4273dbc269b6ad8da8cc6264c82
jbijlsma <jeroen_work@yahoo.com>
2022-02-04 09:17:34 +0100

    Update documentation

--- compose/azurecli/create_cluster.sh
+++ compose/azurecli/create_cluster.sh
@@ -8,20 +8,30 @@
 AKS_RESOURCE_GROUP=rg-dnw
 
 AKS_SERVICE_PRINCIPAL_NAME=sp-dnw
+AKS_SUBSCRIPTION_ID=45dad4eb-e885-48df-a5de-b1f9a02009b0
 AKS_CONTRIBUTOR_ROLE_NAME=Contributor
 AKS_CONTRIBUTOR_ROLE_ID=b24988ac-6180-42a0-ab88-20f7382dd24c
 
+# Only this works with github actions
+# See: https://github.com/Azure/login#configure-deployment-credentials
+az ad sp create-for-rbac \
+  --name $AKS_SERVICE_PRINCIPAL_NAME --role contributor \
+  --scopes /subscriptions/$AKS_SUBSCRIPTION_ID/resourceGroups/$AKS_RESOURCE_GROUP \
+  --sdk-auth
+
 # Create a Service Principal (SP) with password based authentication (password will be in the json output)
-az ad sp create-for-rbac --name $AKS_SERVICE_PRINCIPAL_NAME --role $AKS_CONTRIBUTOR_ROLE_NAME
+# Somehow this does not work
+# az ad sp create-for-rbac --name $AKS_SERVICE_PRINCIPAL_NAME --role $AKS_CONTRIBUTOR_ROLE_NAME
 
 # Get ObjectId based on SP displayname
 # Basic query: az ad sp list --filter "displayname eq '${AKS_SERVICE_PRINCIPAL_NAME}'" --query "[].{displayName:displayName, objectId:objectId}"
 AKS_SERVICE_PRINCIPAL_OBJECT_ID=$(az ad sp list --filter "displayname eq '${AKS_SERVICE_PRINCIPAL_NAME}'" --query "[].{displayName:displayName, objectId:objectId}" | jq -r '.[0].objectId')
 
 # Assign role to Resource Group
-az role assignment create --assignee $AKS_SERVICE_PRINCIPAL_OBJECT_ID \
---role $AKS_CONTRIBUTOR_ROLE_NAME \
---resource-group $AKS_RESOURCE_GROUP
+# This is not needed anymore, because we already specified --scopes with az ad sp create-for-rbac
+# az role assignment create --assignee $AKS_SERVICE_PRINCIPAL_OBJECT_ID \
+# --role $AKS_CONTRIBUTOR_ROLE_NAME \
+# --resource-group $AKS_RESOURCE_GROUP
 
 # Delete the SP
 az ad sp delete --id $AKS_SERVICE_PRINCIPAL_OBJECT_ID
