b9384e4672b58566c7773eebf12b82de6aa9cdf8
jbijlsma <jeroen_work@yahoo.com>
2022-02-08 19:32:45 +0100

    Deploy to DOKS using DOCR

--- compose/doctl/create_cluster.sh
+++ compose/doctl/create_cluster.sh
@@ -5,7 +5,7 @@ DO_CLUSTER_NAME=cluster-dnw \
 DO_NODE_COUNT=1 \
 DO_REGION=fra1 \
 DO_NODE_SIZE=s-2vcpu-4gb \
-DO_REGISTRY_NAME=registry-dnw \
+DO_REGISTRY_NAME=dnw2022 \
 DO_REGISTRY_SUBSCRIPTION_TIER=starter
 
 # Create private container registry
@@ -20,7 +20,7 @@ doctl kubernetes cluster create $DO_CLUSTER_NAME \
 
 # Allow the kubernetes cluster to pull images from the 
 # https://github.com/digitalocean/doctl/issues/847
-doctl kubernetes cluster registry add $DO_CLUSTER_NAME
+doctl kubernetes cluster registry add $DO_REGISTRY_NAME
 # This does not seem to work 
 # doctl registry kubernetes-manifest | kubectl apply -f -
 
@@ -28,3 +28,7 @@ doctl kubernetes cluster registry add $DO_CLUSTER_NAME
 # (1) install ingress-nginx
 # (2) install cert-manager
 # (3) create cert-manager secret with the Cloudflare token
+
+# Cleanup
+# doctl kubernetes cluster registry remove $DO_CLUSTER_NAME
+# doctl registry delete
