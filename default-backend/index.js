aecba5394c7c9076797f421475d5997cc19a1ba9
jbijlsma <jeroen_work@yahoo.com>
2022-02-17 16:15:24 +0100

    Extend default-bakcend with catch all endpoint that returns request url

--- default-backend/index.js
+++ default-backend/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (_, res) => {
-    res.send("Hmmm, it seems you ventured into unknown territory :(");
-});
-
 app.get("/example", (_, res) => {
     res.send("Looking good. This is an example response :)");
+});
+
+app.get("*", (req, res) => {
+    res.send(`Hmmm, it seems you ventured into unknown territory :(. Request: ${req.originalUrl}`);
 });
 
 app.listen(5002, (_) => {
